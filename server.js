c75d39ae505e5d5e47fb3a643d30680b6b72ccc0
Patrick Southier <patrick.southier@hotmail.com>
2022-04-13 19:02:32 -0300

    build: setting up the body-parser to application server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
