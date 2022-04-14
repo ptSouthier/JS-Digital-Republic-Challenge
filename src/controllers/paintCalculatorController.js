221cf7d43a2b1bdc1e7301510a1c35488f4ffd69
Patrick Southier <patrick.southier@hotmail.com>
2022-04-14 01:23:20 -0300

    refactor: moving the business rules related variables declarations to upper scope, outside the functions

--- src/controllers/paintCalculatorController.js
+++ src/controllers/paintCalculatorController.js
@@ -1,5 +1,6 @@
+const room_walls_quantity = 4;
+
 const renderPaintCalculator = (_req, res) => {
-  const room_walls_quantity = 4;
   res.render('pages/paintCalculator', { walls_qty: room_walls_quantity });
 };
 
