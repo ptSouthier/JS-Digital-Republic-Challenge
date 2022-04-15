a456f5b66a8aff621aa12ba682835d43c2ffcbef
Patrick Southier <patrick.southier@hotmail.com>
2022-04-15 16:23:26 -0300

    feat: creating the submitForm function as a controller to a new endpoint

--- src/controllers/paintCalculatorController.js
+++ src/controllers/paintCalculatorController.js
@@ -7,6 +7,25 @@ const renderPaintCalculator = (_req, res) => {
   });
 };
 
+const submitForm = (req, res) => {
+  let paintableAreaSum = 0;
+
+  Object.values(req.body).forEach((wall, index) => {
+    const { height, width, windows, doors } = wall;    
+    const checkEntries = paintCalculatorService.runValidations(height, width, windows, doors, index);
+
+    if (checkEntries) {
+      return res.status(checkEntries.status).json({ message: checkEntries.message });
+    }
+    
+    paintableAreaSum += paintCalculatorService.getWallPaintableArea(height, width, windows, doors);
+  });
+
+  const data = paintCalculatorService.getPaintCansRecommendation(paintableAreaSum);
+  res.status(200).json(data);
+};
+
 module.exports = {
   renderPaintCalculator,
+  submitForm,
 };
